23cae48dddc35257df5f41b1cbf234351cdd5463
Aguszzs <agustinsartori2002@gmail.com>
2024-10-24 14:56:02 -0300

    clase 01 full

---
+++ clase-01/src/main/java/com/coderhouse/Clase01.java
@@ -0,0 +1,40 @@
+package com.coderhouse;
+
+public class Clase01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		byte   variable = 10;
+		
+		byte var2 = 2;
+		
+		System.out.println("asdasd " + (variable + var2));
+		
+		
+		float num1 = 12f;
+		float num2 = 12f;
+		float res;
+		
+		// calc
+		
+		res = num1 + num2;
+		System.out.println("res = " + res);
+	
+		//incrementos 
+		
+		int contador = 1;
+		
+		System.out.println(contador);
+		contador++;
+		System.out.println(contador);
+		contador--;
+		System.out.println(contador);
+	
+		/* == es para vert si son del mismo valor
+		 * === es para saber lo mismo pero tambien si son del mismo tipo*/
+		
+		
+	}
+
+}
